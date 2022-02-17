7566c1062eb82aef5ccb98415c17bbd32646e72f
filipeqm94 <queiroz.filipe94@gmail.com>
2022-02-17 12:12:42 -0500

    remove unused code

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 function Footer() {
-  return <footer className='flex justify-center p-5 bg-[#333] min-w-fit sticky'>
-      <small className=''>Filipe Marques &copy; 2022</small>
-  </footer>;
+  return (
+    <footer className='flex justify-center p-5 bg-[#333] min-w-fit sticky'>
+      <small>Filipe Marques &copy; 2022</small>
+    </footer>
+  )
 }
 
-export default Footer;
+export default Footer
